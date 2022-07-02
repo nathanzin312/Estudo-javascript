7407a1c02be45a9d38da645dc4e1fd8127b22b71
Nathan <nathansanches890@gmail.com>
2022-07-02 13:29:07 -0300

    13 commit

--- array/array.js
+++ array/array.js
@@ -6,3 +6,14 @@ let frutas1 = [];
 frutas1[3] = 'abacaxi';
 frutas1.push('limao');
 console.log(frutas1)
+
+
+const nomes = ['luzia','marcela','maria'];
+nomes.push('kamylli');
+
+nomes.unshift('bruna');
+
+let carros1 = ['uno','gol','chevette','saveiro','astra']
+let novoArray = carros1.slice(2,4)
+delete carros1[1]
+console.log(carros1);
